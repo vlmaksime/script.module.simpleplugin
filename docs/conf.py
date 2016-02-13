65f1060862dc69fa8e6ffdf3004dee022be953c6
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-13 12:01:35 +0200

    Fixes docs generating without kodistubs

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,8 @@ import sys
 import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), 'script.module.simpleplugin', 'libs'))
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
-                                'xbmcstubs'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__))))
+import tests
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
